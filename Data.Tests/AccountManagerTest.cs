001baec8531101439f522cd808479de94e4b2ee5
Kevin Hicks <keyholesoftware@gmail.com>
2013-06-23 19:43:17 -0400

    Added Test To Data.Test

---
+++ Data.Tests/AccountManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Tests
+{
+    [TestClass]
+    public class AccountManagerTest
+    {
+        [TestInitialize]
+        public void TestStartUp()
+        {
+        }/*
+
+        [TestMethod]
+        public void CreateUser()
+        {
+            var accountManager = new AccountManager();
+
+            string result = accountManager.CreateAccount("aosidn", "aosidj");
+
+            Assert.IsNotNull(result);
+        }*/
+
+    }
+}
---
+++ Data.Tests/TaxCalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Tests
+{
+    [TestClass]
+    public class TaxCalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
