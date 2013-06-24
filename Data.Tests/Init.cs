9e454160a432cab7a41bc1183ceddea689014c3f
Kevin Hicks <keyholesoftware@gmail.com>
2013-06-24 16:08:30 -0400

    Added Test Initializer

---
+++ Data.Tests/Init.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Data.Tests
+{
+    [TestClass()]
+    class TestsInit
+    {
+        [AssemblyInitialize()]
+        public static void AllTestsInitializer(TestContext context)
+        {
+            new ApplicationDBContext().UpdateDatabase();
+        }
+    }
+}
