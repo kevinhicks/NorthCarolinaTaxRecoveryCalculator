85fe77660153de3f1132d7e29082e13b7701b18a
Kevin Hicks <keyholesoftware@gmail.com>
2013-07-27 07:49:17 -0400

    working on Payment Vouchers

---
+++ NorthCarolinaTaxRecoveryCalculator/Controllers/PaymentVoucherController.cs
@@ -0,0 +1,28 @@
+﻿using NorthCarolinaTaxRecoveryCalculator.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthCarolinaTaxRecoveryCalculator.Controllers
+{
+    public class PaymentVoucherController : Controller
+    {
+        //
+        // GET: /PaymentVoucher/
+
+        public ActionResult Index()
+        {
+            var paymentVoucher = new PaymentVoucher();
+            var entry = new PaymentVoucherEntry();
+            entry.Item = "Bolt";
+            entry.CostElement = "asd";
+            entry.Amount = 100;
+
+            paymentVoucher.Entries.Add(entry);
+            return View(paymentVoucher);
+        }
+
+    }
+}
---
+++ NorthCarolinaTaxRecoveryCalculator/Models/Data/PaymentVoucherModels.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace NorthCarolinaTaxRecoveryCalculator.Models.Data
+{
+    public class PaymentVoucher
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public string CheckNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string PaidTo { get; set; }
+
+        public List<PaymentVoucherEntry> Entries {get;set;}
+
+        public string PreparedBy { get; set; }
+        public string ApprovedBy { get; set; }
+        public string RBCApproval { get; set; }
+        
+        public PaymentVoucher()
+        {
+            ID = Guid.NewGuid();
+            Date = DateTime.Now;
+
+            Entries = new List<PaymentVoucherEntry>();
+        }
+    }
+
+    public class PaymentVoucherEntry
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        public Guid PaymentVoucherID { get; set; }
+
+        public string Item { get; set; }
+        public string CostElement { get; set; }
+        public float Amount { get; set; }
+
+        public PaymentVoucherEntry()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
