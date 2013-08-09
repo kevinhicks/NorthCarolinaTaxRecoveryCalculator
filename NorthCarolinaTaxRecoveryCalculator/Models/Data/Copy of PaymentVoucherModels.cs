a5852ccd1522c03aee2ca4c7e911feff2986f224
Kevin Hicks <keyholesoftware@gmail.com>
2013-08-09 09:54:01 -0400

    playing around with Generics

---
+++ NorthCarolinaTaxRecoveryCalculator/Models/Data/Copy of PaymentVoucherModels.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Linq;
+using NorthCarolinaTaxRecoveryCalculator.Misc;
+
+namespace NorthCarolinaTaxRecoveryCalculator.Models.Data
+{
+    public class Smurf : ITable
+    {
+    }
+
+    public class Hobbit : ITable
+    {
+    }
+
+    public abstract class ITable
+    {
+        public Guid ID { get; set; }
+    }
+
+    public interface IRepository<T> where T : ITable
+    {
+        void Save(T t);
+        T Get();
+        IEnumerable<T> GetAll();
+        void Delete(T t);
+    }
+
+    public class HobbitThing : IRepository<Hobbit>
+    {
+        public void Save(Hobbit t)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public Hobbit Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hobbit> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Hobbit t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SmurfThing : IRepository<Smurf>
+    {
+        public void Save(Smurf t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Smurf Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Smurf> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Smurf t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
